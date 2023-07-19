772560216a26bbf4c6234a08a8ea67f0280d1ed9
sitinurmianti4599 <sitinurmianti4599@gmail.com>
2023-07-20 07:28:08 +0800

    add link

--- resources/views/cek.blade.php
+++ resources/views/cek.blade.php
@@ -4,7 +4,7 @@
 @section('konten')
     <div class="title-cek">
         <div class="cover-cek">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand site_title" href="/">
                 <img src="assets/images/logohd.png" alt="Logo" height="40" class="d-inline-block align-text-top">
             </a>
             <h2 class="">Cek Progress Berkas Layanan <b>Hukum</b> Anda</h2>
